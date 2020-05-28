834bf4cd8cf480518d68c9483f8a6b855fcafb7e
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-05-28 16:48:47 -0400

    implemented update board

--- board.py
+++ board.py
@@ -4,12 +4,12 @@ import curses
 import time
 
 class Board:
-    def __init__(self, rows, columns):
+    def __init__(self, odds):
         self.window = curses.initscr()
         self.rows, self.col = self.window.getmaxyx()
         self.grid = [[Cell() for i in range(self.col)] for j in range(self.rows)]
 
-        self._initiate_random_board(1)
+        self._initiate_random_board(odds)
 
     def _initiate_random_board(self, odds):
         for i in self.grid:
@@ -19,19 +19,19 @@ class Board:
                     j.set_alive()
 
     def draw_board(self):
-        #curses.curs_set(0)
         curses.start_color()
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-        self.window.attron(curses.color_pair(1))
         for i in range(self.rows - 1):
             for j in range(self.col):
-                self.window.addstr(i,j," ")
-                time.sleep(.01)
-                self.window.refresh()
-        self.window.attroff(curses.color_pair(1))
+                if (self.grid[i][j].alive):
+                    self.window.attron(curses.color_pair(1))
+                    self.window.addstr(i,j,' ')
+                    self.window.attroff(curses.color_pair(1))
+        self.window.refresh()
+        time.sleep(1)
         curses.endwin()
 
 
-thing = Board(10,10)
+thing = Board(5)
 thing.draw_board()
