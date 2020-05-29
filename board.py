eccfb5482738d673cfd4bfa05f7842c1359170a0
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-05-29 12:49:10 -0400

    Added glider gun option

--- board.py
+++ board.py
@@ -2,9 +2,10 @@ from copy import deepcopy
 import curses
 from random import randint
 import time
+import sys
 
 class Board:
-    def __init__(self, odds, time, type):
+    def __init__(self, odds, gens, type):
         # Set the size of the simulation to the size of the terminal
         self.window = curses.initscr()
         self.rows, self.col = self.window.getmaxyx()
@@ -13,12 +14,31 @@ class Board:
             self._initiate_random_board(odds)
         elif type == 2:
             self._initiate_glider()
-            ########### ADD OPTION
+        else:
+            sentence = "Note: because of the wraparound nature of the board, the glider will eventually destroy itself       \n"
+            for char in sentence:
+                time.sleep(0.05)
+                sys.stdout.write(char)
+                sys.stdout.flush()
+            self._initiate_glider_gun()
         # Run the simulation for time generations
-        for i in range(time):
+        for i in range(gens):
             self.draw_board()
             self.update_board_normal()
         curses.endwin()
+
+    def set(self, i, j):
+        self.grid[i][j] = 1
+
+    def create_column(self, i, j, size):
+        for k in range(size):
+            self.grid[i + k][j] = 1
+
+    def create_square(self, i, j):
+        self.grid[i][j] = 1
+        self.grid[i][j + 1] = 1
+        self.grid[i + 1][j] = 1
+        self.grid[i + 1][j + 1] = 1
 
     # Create's a totally random board based on the inputted odds
     def _initiate_random_board(self, odds):
@@ -40,6 +60,37 @@ class Board:
         for i in range(int(self.col/6)):
             for j in range(int(self.rows/5)):
                 create_glider(j * 5, i * 6)
+
+    def _initiate_glider_gun(self):
+
+        def create_gun_one(i, j):
+            self.set(i, j + 2)
+            self.set(i, j + 3)
+            self.set(i + 1, j + 1)
+            self.set(i + 1, j + 5)
+            self.create_column(i + 2, j, 3)
+            self.set(i + 3, j + 4)
+            self.create_column(i + 2, j + 6, 3)
+            self.set(i + 3, j + 7)
+            self.set(i + 5, j + 1)
+            self.set(i + 5, j + 5)
+            self.set(i + 6, j + 2)
+            self.set(i + 6, j + 3)
+
+        def create_gun_two(i, j):
+            self.create_column(i + 2, j, 3)
+            self.create_column(i + 2, j + 1, 3)
+            self.set(i + 1, j + 2)
+            self.set(i + 5, j + 2)
+            self.create_column(i, j + 4, 2)
+            self.create_column(i + 5, j + 4, 2)
+
+        create_gun_one(2, 10)
+        create_gun_two(0, 20)
+        self.create_square(4, 0)
+        self.create_square(2, 34)
+
+
 
     # Adds up how many of a tile's naighbors are alive
     def check_neighbors(self, r, c):
@@ -91,4 +142,4 @@ class Board:
                 else:
                     self.window.addstr(i,j,' ')
         self.window.refresh()
-        time.sleep(.1)
+        time.sleep(.01)
